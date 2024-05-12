e034722e8cc1f08897aaeaaac672d4f986b51415
Nadia <ndecar@vigigo.com>
2024-05-12 14:20:32 -0400

    Pequeño cambio en la ruta

--- recetas/urls.py
+++ recetas/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('/recetas/', views.listado_recetas, name='recetas')
+    path('/recetas', views.listado_recetas, name='recetas')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
