cf391f065823bf16f8565bf83b53fd1701632165
Nadia <nadiadecar@ug.uchile.cl>
2024-05-29 02:14:34 -0400

    La última presentación no evaluada

--- recetas/urls.py
+++ recetas/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('/recetas', views.listado_recetas, name='recetas'),
-    path('/receta/<int:id>', views.ver_receta, name='receta'),
+    path('recetas', views.listado_recetas, name='recetas'),
+    path('receta/<int:id>', views.ver_receta, name='receta'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
